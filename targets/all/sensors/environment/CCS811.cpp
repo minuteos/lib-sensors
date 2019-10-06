067b8e45798854e94835859350da64875db346a3
Štefan Šimek <simek@triaxis.sk>
2019-10-06 09:38:15 +0200

    add ams CCS811 air quality (CO2 and tVOC) sensor

---
+++ targets/all/sensors/environment/CCS811.cpp
@@ -0,0 +1,234 @@
+/*
+ * Copyright (c) 2019 triaxis s.r.o.
+ * Licensed under the MIT license. See LICENSE.txt file in the repository root
+ * for full license information.
+ *
+ * sensors/environment/CCS811.cpp
+ */
+
+#include "CCS811.h"
+
+#define MYDBG(...)  DBGCL("CCS811", __VA_ARGS__)
+
+namespace sensors::environment
+{
+
+async(CCS811::ReadRegister, Register reg, Buffer buf)
+async_def()
+{
+    if (await(i2c.Write, address, reg, true, false) != 1)
+    {
+        MYDBG("Failed to write register %02X address", reg);
+        async_return(false);
+    }
+
+    size_t len = await(i2c.Read, address, buf, false, true);
+    if (len != buf.Length())
+    {
+        MYDBG("Failed to read register %02X value, %d out of %d bytes read", reg, len, buf.Length());
+        async_return(false);
+    }
+
+    async_return(true);
+}
+async_end
+
+
+async(CCS811::WriteRegister, Register reg, Span buf)
+async_def()
+{
+    if (await(i2c.Write, address, reg, true, buf.Length() == 0) != 1)
+    {
+        MYDBG("Failed to write register %02X address", reg);
+        async_return(false);
+    }
+
+    if (buf.Length())
+    {
+        size_t len = await(i2c.Write, buf, true);
+        if (len != buf.Length())
+        {
+            MYDBG("Failed to write register %02X value, %d out of %d bytes read", reg, len, buf.Length());
+            async_return(false);
+        }
+    }
+
+    async_return(true);
+}
+async_end
+
+async(CCS811::SetMode, DriveMode mode)
+async_def()
+{
+    if (this->mode != mode)
+    {
+        this->mode = mode;
+        if (init)
+        {
+            // device initialized, try to change mode immediately
+            wake.Res();
+            async_delay_ms(1);
+            msg.mode.raw = 0;
+            msg.mode.driveMode = (uint8_t)mode;
+            DBG("Changing mode to %d", mode);
+            init = await(WriteRegister, Register::Mode, msg.mode);
+            wake.Set();
+        }
+    }
+}
+async_end
+
+async(CCS811::Init)
+async_def()
+{
+    MYDBG("Resetting...");
+    async_yield();
+    wake.Res();
+    reset.Res();
+    async_delay_ms(1);
+    reset.Set();
+    async_delay_ms(1);
+
+    MYDBG("Initializing...");
+
+    if (!await(ReadRegister, Register::HWID, msg.hwId))
+        async_return(false);
+
+    if (msg.hwId != HwID::CCS811)
+    {
+        MYDBG("Unexpected HWID value: %02X, expected %02X", msg.hwId, HwID::CCS811);
+        async_return(false);
+    }
+
+    for (;;)
+    {
+        if (!await(ReadRegister, Register::Status, msg.status))
+            async_return(false);
+
+        MYDBG("STATUS: %02X", msg.status.raw);
+        if (msg.status.appRunning)
+            break;
+
+        MYDBG("Firmware in boot mode, starting application");
+        if (!await(WriteRegister, Register::BootAppStart))
+            async_return(false);
+        async_delay_ms(10);
+    }
+
+    if (!await(ReadRegister, Register::Mode, msg.mode))
+        async_return(false);
+
+    MYDBG("MODE: %d %c%c", msg.mode.driveMode, msg.mode.intDataReady ? 'I' : '-', msg.mode.intThreshold ? 'T': '-');
+    if (msg.mode.driveMode != (uint8_t)mode)
+    {
+        MYDBG("Setting mode %d", mode);
+        msg.mode.driveMode = (uint8_t)mode;
+        if (!await(WriteRegister, Register::Mode, msg.mode))
+            async_return(false);
+    }
+
+#if TRACE
+    if (!await(ReadRegister, Register::HWVersion, msg.hwVer))
+        async_return(false);
+
+    MYDBG("HWVER: %d.%d", msg.hwVer.major, msg.hwVer.build);
+
+    if (!await(ReadRegister, Register::FWBootVersion, msg.bootVer))
+        async_return(false);
+
+    MYDBG("BOOT: %d.%d.%d", msg.bootVer.major, msg.bootVer.minor, msg.bootVer.trivial);
+
+    if (!await(ReadRegister, Register::FWAppVersion, msg.appVer))
+        async_return(false);
+
+    MYDBG("APP: %d.%d.%d", msg.appVer.major, msg.appVer.minor, msg.appVer.trivial);
+
+    if (!await(ReadRegister, Register::ErrorID, msg.error))
+        async_return(false);
+
+    MYDBG("ERROR: %d", msg.error);
+#endif
+
+    wake.Set();
+    async_return(init = true);
+}
+async_end
+
+async(CCS811::Measure)
+async_def(bool success; int i)
+{
+    if (init || await(Init))
+    {
+        wake.Res();
+        async_delay_ms(1);
+
+        if (mode == DriveMode::Idle)
+        {
+            // enable the fastest mode for one measurement
+            msg.mode.raw = 0;
+            msg.mode.driveMode = (uint8_t)DriveMode::ConstantPower_250ms;
+            if (await(WriteRegister, Register::Mode, msg.mode))
+            {
+                // measurement takes at least one second
+                async_delay_ms(980);
+                // wait up to another second for the result
+                for (f.i = 0; f.i < 50; f.i++)
+                {
+                    async_delay_ms(20);
+                    if (!await(ReadRegister, Register::Result, msg.result))
+                        continue;
+
+                    if (msg.result.status.error)
+                    {
+                        MYDBG("ERROR %d", msg.result.error);
+                        break;
+                    }
+
+                    if (msg.result.status.dataReady)
+                    {
+                        f.success = true;
+                        co2 = FROM_BE16(msg.result.co2BE);
+                        tvoc = FROM_BE16(msg.result.tvocBE);
+                        MYDBG("new data: CO2=%dppm, TVOC=%dppb, RAW I=%duA, ADC=%d",
+                            (int)co2, (int)tvoc, msg.result.raw.current, (msg.result.raw.adcHi) << 8 | msg.result.raw.adcLo);
+                        break;
+                    }
+                }
+
+                msg.mode.raw = 0;
+                msg.mode.driveMode = (uint8_t)DriveMode::Idle;
+                await(WriteRegister, Register::Mode, msg.mode);
+            }
+        }
+        else if (await(ReadRegister, Register::Result, msg.result))
+        {
+            if (msg.result.status.error)
+            {
+                MYDBG("ERROR %d", msg.result.error);
+            }
+            else
+            {
+                f.success = true;
+                co2 = FROM_BE16(msg.result.co2BE);
+                tvoc = FROM_BE16(msg.result.tvocBE);
+                MYDBG("%s data: CO2=%dppm, TVOC=%dppb, RAW I=%duA, ADC=%d",
+                    msg.result.status.dataReady ? "new" : "old",
+                    (int)co2, (int)tvoc, msg.result.raw.current, (msg.result.raw.adcHi) << 8 | msg.result.raw.adcLo);
+            }
+        }
+    }
+
+    if (!f.success)
+    {
+        MYDBG("Measurement FAILED");
+        init = false;
+        co2 = tvoc = NAN;
+    }
+
+    wake.Set();
+    async_return(f.success && msg.result.status.dataReady);
+}
+async_end
+
+}
+
---
+++ targets/all/sensors/environment/CCS811.h
@@ -0,0 +1,240 @@
+/*
+ * Copyright (c) 2019 triaxis s.r.o.
+ * Licensed under the MIT license. See LICENSE.txt file in the repository root
+ * for full license information.
+ *
+ * sensors/environment/CCS811.h
+ */
+
+#pragma once
+
+#include <kernel/kernel.h>
+
+#include <bus/I2C.h>
+
+#include <hw/GPIO.h>
+
+namespace sensors::environment
+{
+
+//! Driver for ams CCS811 CO2/VOC sensor
+class CCS811
+{
+public:
+    enum struct Address : uint8_t
+    {
+        Low = 0x5A,
+        High = 0x5B,
+    };
+
+    enum struct DriveMode : uint8_t
+    {
+        Idle = 0,
+        ConstantPower_1s = 1,
+        PulseHeating_10s = 2,
+        LowPowerPulse_60s = 3,
+        ConstantPower_250ms = 4,
+    };
+
+    CCS811(bus::I2C& i2c, Address address = Address::Low, GPIOPin wake = Px, GPIOPin reset = Px)
+        : i2c(i2c), address((uint8_t)address), wake(wake), reset(reset)
+    {
+    }
+
+    //! Initializes the sensors and configures the selected mode
+    async(Init);
+    //! Retrieves the last measurement result, return value indicates if the measured values have changed in the meantime
+    //! If current mode is @ref Idle, the sensor is temporary switched to mode 4 and waits for one cycle to complete
+    async(Measure);
+    //! Sets the measurement mode. If the sensor is already initialized, change is applied immediately
+    async(SetMode, DriveMode mode);
+    //! Gets the currently requested measurement mode
+    DriveMode GetMode() const { return mode; }
+
+    //! Gets the last CO2 measurement in ppm; NaN if not available
+    float GetCO2ppm() const { return co2; }
+    //! Gets the last tVOC measurement in ppb; NaN if not available
+    float GetTVOCppb() const { return tvoc; }
+
+private:
+    bus::I2C& i2c;
+    uint8_t address;
+    DriveMode mode = DriveMode::Idle;
+    bool init = false;
+    GPIOPin wake, reset;
+    float co2 = NAN, tvoc = NAN;
+
+    enum struct Register : uint8_t
+    {
+        Status = 0x00,
+        Mode = 0x01,
+        Result = 0x02,
+        Raw = 0x03,
+        EnvData = 0x05,
+        Thresholds = 0x10,
+        Baseline = 0x11,
+        HWID = 0x20,
+        HWVersion = 0x21,
+        FWBootVersion = 0x23,
+        FWAppVersion = 0x24,
+        ErrorID = 0xE0,
+        BootAppErase = 0xF1,
+        BootAppData = 0xF2,
+        BootAppVerify = 0xF3,
+        BootAppStart = 0xF4,
+        SoftReset = 0xFF,
+    };
+
+    enum struct HwID : uint8_t
+    {
+        CCS811 = 0x81,
+    };
+
+    enum struct Error : uint8_t
+    {
+        //! Attempt to write to an invalid register
+        InvalidWrite,
+        //! Attempt to read from an invalid register
+        InvalidRead,
+        //! Invalid mode value written
+        InvalidMode,
+        //! Measured value exceeded range
+        MaxResistance,
+        //! Heater failed
+        HeaterFault,
+        //! Heater supply failed
+        HeaterSupply,
+    };
+
+    enum struct ResetKey : uint32_t
+    {
+        Boot = 0x8A72E511,
+    };
+
+    enum struct AppEraseKey : uint32_t
+    {
+        Key = 0x09E6A7E7,
+    };
+
+    enum struct AppVerifyKey : uint8_t
+    {
+        Key = 0,
+    };
+
+    union Status
+    {
+        uint8_t raw;
+        struct
+        {
+            uint8_t error : 1;
+            uint8_t : 2;
+            uint8_t dataReady : 1;
+            uint8_t appValid : 1;
+            uint8_t appVerify : 1;
+            uint8_t appErase : 1;
+            uint8_t appRunning : 1;
+        };
+    };
+
+    union Mode
+    {
+        uint8_t raw;
+        struct
+        {
+            uint8_t : 2;
+            uint8_t intThreshold : 1;
+            uint8_t intDataReady : 1;
+            uint8_t driveMode : 3;
+            uint8_t : 1;
+        };
+    };
+
+    struct RawData
+    {
+        //! Top two bits of raw ADC value
+        uint8_t adcHi : 2;
+        //! Current in microamperes
+        uint8_t current : 6;
+        //! Bottom eight bits of raw ADC value
+        uint8_t adcLo;
+    };
+
+    struct EnvData
+    {
+        //! Big endian relative humidity in 1/512% steps, [0,51200] => [0%,100%] RH
+        uint16_t humBE;
+        //! Big endian temperature in 1/512 degree steps, [0,65536) => [-25,102) deg. C
+        uint16_t tempBE;
+    };
+
+    union Result
+    {
+        uint8_t data[8];
+        struct
+        {
+            //! Big endian measured CO2 level in ppm
+            uint16_t co2BE;
+            //! Big endian measured TVOC level in ppb
+            uint16_t tvocBE;
+            Status status;
+            Error error;
+            RawData raw;
+        };
+    };
+
+    struct Thresholds
+    {
+        //! Big endian threshold between low and medium CO2 concentration in ppm (default 1500)
+        uint16_t lowMedBE;
+        //! Big endian threshold between medium and high CO2 concentration in ppm (default 2500)
+        uint16_t medHighBE;
+    };
+
+    struct HwVersion
+    {
+        //! Build variant
+        uint8_t build : 4;
+        //! Major hardware version, should be 1
+        uint8_t major : 4;
+    };
+
+    struct FwVersion
+    {
+        //! Minor version
+        uint8_t minor : 4;
+        //! Major version
+        uint8_t major : 4;
+        //! Trivial version (revision)
+        uint8_t trivial;
+    };
+
+    union
+    {
+        Status status;
+        Mode mode;
+        Result result;
+        RawData raw;
+        EnvData env;
+        Thresholds thr;
+        //! Calculation baseline with unspecified format
+        //! Can be stored after calibration in clean air and restored later
+        uint16_t baseline;
+        //! Hardware ID
+        HwID hwId;
+        //! Hardware version
+        HwVersion hwVer;
+        //! Bootloader version
+        FwVersion bootVer;
+        //! Firmware version
+        FwVersion appVer;
+        Error error;
+        ResetKey reset;
+        AppEraseKey appErase;
+        AppVerifyKey appVerify;
+    } msg;
+
+    async(ReadRegister, Register reg, Buffer buf);
+    async(WriteRegister, Register reg, Span buf = Span());
+};
+
+}
