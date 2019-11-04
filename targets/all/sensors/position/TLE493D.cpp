78f9e1ab1eef88ee28ba990bc23c47c012c24a8e
Štefan Šimek <simek@triaxis.sk>
2019-11-04 09:31:14 +0100

    TLE439D: 3D Hall sensor

---
+++ targets/all/sensors/position/TLE493D.cpp
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2019 triaxis s.r.o.
+ * Licensed under the MIT license. See LICENSE.txt file in the repository root
+ * for full license information.
+ *
+ * sensors/position/TLE493D.cpp
+ */
+
+#include "TLE493D.h"
+
+#define MYDBG(...)  DBGCL("TLE493D", __VA_ARGS__)
+
+namespace sensors::position
+{
+
+async(TLE493D::Init)
+async_def(
+    union
+    {
+        struct
+        {
+            uint8_t _discard_data_regs[6];
+            Diagnostics diag;
+            uint8_t _discard_wake_regs[9];
+            Config cfg;
+            Mode1 mode1;
+            uint8_t _reserved12;
+            uint8_t _reserved13 : 5;
+            uint8_t prd : 3;
+            uint8_t _reserved14, _reserved15;
+            uint8_t rev : 4;
+            uint8_t feat: 2;
+            uint8_t : 2;
+        } data;
+        struct
+        {
+            Register reg;
+            Config cfg;
+            Mode1 mode1;
+        } init;
+    };
+)
+{
+    // we must start by configuring the protocol of the sensor, as it stars in a mode where reads are not I2C compatible
+    MYDBG("Configuring...");
+    f.init.reg = Register::Config;
+    f.init.cfg = Config::TemperatureCompensationOff | Config::ReadTriggerBefore | Config::DisableTemperature;
+    f.init.mode1 = Mode1::InterruptDisable | Mode1::PowerModeTrigger | Mode1::Protocol1Byte | Mode1Address();
+    f.init.mode1 = f.init.mode1 | (!__builtin_parity((uint8_t)f.init.mode1) * Mode1::FuseParity);
+
+    // double configuration to verify the device did not stop communicatings
+    if (await(i2c.Write, address, f.init, true, true) != sizeof(f.init) ||
+        await(i2c.Write, address, f.init, true, true) != sizeof(f.init))
+    {
+        MYDBG("Failed to configure sensor");
+        async_return(false);
+    }
+
+    async_delay_ms(1);
+
+    MYDBG("Reading registers...");
+    if (await(i2c.Read, address, f.data, true, true) != sizeof(f.data))
+    {
+        MYDBG("Failed to read registers register");
+        async_return(false);
+    }
+
+    if (!(f.data.diag & Diagnostics::FuseParity))
+    {
+        MYDBG("Fuse parity error, sensor not functional");
+        async_return(false);
+    }
+
+    MYDBG("DIAG: %02X, PRD: %d, FEAT: %d, REV: %d", f.data.diag, f.data.prd, f.data.feat, f.data.rev);
+    async_return(init = true);
+}
+async_end
+
+async(TLE493D::Measure)
+async_def(
+    struct
+    {
+        int8_t bx, by, bz;
+        uint8_t temp;
+        uint8_t byl : 4;
+        uint8_t bxl : 4;
+        uint8_t bzl : 4;
+        uint8_t id : 2;
+        uint8_t templ : 2;
+        Diagnostics diag;
+    } data;
+)
+{
+    if (!init && !await(Init))
+    {
+        async_return(false);
+    }
+
+    if (await(i2c.Read, address, f.data, true, true) != sizeof(f.data))
+    {
+        MYDBG("Failed to read measurement");
+        async_return(false);
+    }
+
+    x = (int32_t(((f.data.bx << 4) | f.data.bxl) << 20) >> 20) * ValueMultiply;
+    y = (int32_t(((f.data.by << 4) | f.data.byl) << 20) >> 20) * ValueMultiply;
+    z = (int32_t(((f.data.bz << 4) | f.data.bzl) << 20) >> 20) * ValueMultiply;
+    MYDBG("new data: X=%.1q Y=%.1q Z=%.1q", int(x * 10), int(y * 10), int(z * 10));
+    async_return(true);
+}
+async_end
+
+}
---
+++ targets/all/sensors/position/TLE493D.h
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2019 triaxis s.r.o.
+ * Licensed under the MIT license. See LICENSE.txt file in the repository root
+ * for full license information.
+ *
+ * sensors/position/TLE493D.h
+ */
+
+#pragma once
+
+#include <kernel/kernel.h>
+
+#include <bus/I2C.h>
+
+namespace sensors::position
+{
+
+class TLE493D
+{
+public:
+    enum struct Address : uint8_t
+    {
+        A0 = 0x35,  // 0b011 0101
+        A1 = 0x22,  // 0b010 0010
+        A2 = 0x78,  // 0b111 1000
+        A3 = 0x44,  // 0b100 0100
+    };
+
+    TLE493D(bus::I2C& i2c, Address address)
+        : i2c(i2c), address((uint8_t)address)
+    {
+    }
+
+    //! Field intensity in X direction in mT
+    float GetFieldX() const { return x; }
+    //! Field intensity in Y direction in mT
+    float GetFieldY() const { return y; }
+    //! Field intensity in Z direction in mT
+    float GetFieldZ() const { return z; }
+
+    //! Initializes the sensor
+    async(Init);
+    //! Retrieves the last measurement result, return value indicates if the measured values have changed in the meantime
+    async(Measure);
+
+private:
+    enum struct Register : uint8_t
+    {
+        Bx = 0, By = 1, Bz = 2,
+        Temp = 3, Bx2 = 4, Temp2 = 5,
+        Diagnostics = 6,
+
+        WakeXLow = 7,
+        WakeXHigh = 8,
+        WakeYLow = 9,
+        WakeYHigh = 0xA,
+        WakeZLow = 0xB,
+        WakeZHigh = 0xC,
+
+        WakeXLSB = 0xD,
+        WakeYLSB = 0xE,
+        WakeZLSB = 0xF,
+
+        Config = 0x10,
+        Mode1 = 0x11,
+        Mode2 = 0x13,
+        Version = 0x16,
+    };
+
+    enum struct Diagnostics : uint8_t
+    {
+        FrameMask = 0x03,
+        BxDone = 0x04,
+        TempDone = 0x08,
+        TestMode = 0x10,
+        ConfigParity = 0x20,
+        FuseParity = 0x40,
+        BusParity = 0x80,
+    };
+
+    enum struct Config : uint8_t
+    {
+        Parity = 1,
+
+        TemperatureCompensationOff = 0,
+        TemperatureCompensation1 = 1 << 1,
+        TemperatureCompensation2 = 2 << 1,
+        TemperatureCompensation3 = 3 << 1,
+        ShortRange = 8,
+
+        ReadTriggerOff = 0,
+        ReadTriggerBefore = 0x10,
+        ReadTriggerAfter = 0x20,
+
+        DisableZ = 0x40,
+        DisableTemperature = 0x80,
+    };
+
+    enum struct Mode1 : uint8_t
+    {
+        PowerModeAuto = 0,
+        PowerModeTrigger = 1,
+        PowerModeContinuous = 3,
+
+        InterruptDisable = 4,
+        CollisionAvoidanceDisable = 8,
+
+        Protocol1Byte = 0x10,
+
+        Address0 = 0 << 5,
+        Address1 = 1 << 5,
+        Address2 = 2 << 5,
+        Address3 = 3 << 5,
+
+        FuseParity = 0x80,
+    };
+
+    DECLARE_FLAG_ENUM(Diagnostics);
+    DECLARE_FLAG_ENUM(Config);
+    DECLARE_FLAG_ENUM(Mode1);
+
+    //! Returns the configred address in Mode1 register format
+    Mode1 Mode1Address() const { return (Mode1::Address2 * GETBIT(address, 6)) | (Mode1::Address1 * !GETBIT(address, 4)); }
+
+    bus::I2C& i2c;
+    uint8_t address;
+    bool init = false;
+    float x = NAN, y = NAN, z = NAN;
+
+    static constexpr float ValueMultiply = 200 / 2048.0;
+};
+
+DEFINE_FLAG_ENUM(TLE493D::Diagnostics);
+DEFINE_FLAG_ENUM(TLE493D::Config);
+DEFINE_FLAG_ENUM(TLE493D::Mode1);
+
+}
