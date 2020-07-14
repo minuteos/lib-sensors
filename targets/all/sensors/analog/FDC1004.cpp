e659761133aa1850043e37d857c339867125fe26
Štefan Šimek <simek@triaxis.sk>
2020-07-14 22:21:19 +0200

    FDC1004: capacitance sensor (untested code)

---
+++ targets/all/sensors/analog/FDC1004.cpp
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2020 triaxis s.r.o.
+ * Licensed under the MIT license. See LICENSE.txt file in the repository root
+ * for full license information.
+ *
+ * sensors/analog/FDC1004.cpp
+ */
+
+#include "FDC1004.h"
+
+namespace sensors::analog
+{
+
+async(FDC1004::Init)
+async_def(
+    uint16_t value;
+    FDCConfig fdcConf;
+)
+{
+    init = false;
+    if (!await(ReadRegister, Register::DEVICE_ID, f.value))
+    {
+        async_return(false);
+    }
+
+    if (f.value != DeviceID)
+    {
+        MYDBG("Unexpected ID: %04X, expected %04X", f.value, DeviceID);
+        async_return(false);
+    }
+
+    f.fdcConf = FDCConfig::Reset;
+    if (!await(WriteRegister, Register::FDC_CONF, f.fdcConf))
+    {
+        async_return(false);
+    }
+
+    MYDBG("Reset...");
+    for (;;)
+    {
+        if (!await(ReadRegister, Register::FDC_CONF, f.fdcConf))
+        {
+            async_return(false);
+        }
+
+        if (!(f.fdcConf & FDCConfig::Reset))
+        {
+            break;
+        }
+
+        async_yield();
+    }
+
+    init = true;
+    configuredChannels = 0;
+    async_return(true);
+}
+async_end
+
+async(FDC1004::Configure, unsigned channel, uint16_t cfg)
+async_def()
+{
+    ASSERT(channel < countof(value));
+
+    if (!await(WriteRegister, (uint8_t)Register::CONF_MEAS1 + channel, cfg))
+    {
+        async_return(false);
+    }
+
+    SETBIT(configuredChannels, channel);
+    async_return(true);
+}
+async_end
+
+async(FDC1004::SetCalibration, unsigned input, OffsetAndGain arg)
+async_def()
+{
+    async_return(
+        await(WriteRegister, (uint8_t)Register::OFFSET_CAL_CIN1 + input, arg.offset) &&
+        await(WriteRegister, (uint8_t)Register::GAIN_CAL_CIN1 + input, arg.gain)
+    );
+}
+async_end
+
+async(FDC1004::Start, FDCConfig config)
+async_def()
+{
+    async_return(await(WriteRegister, Register::FDC_CONF, config));
+}
+async_end
+
+async(FDC1004::Wait, Timeout timeout)
+async_def(
+    Timeout timeout;
+    FDCConfig config;
+)
+{
+    f.timeout = timeout.MakeAbsolute();
+    
+    do
+    {
+        if (!await(ReadRegister, Register::FDC_CONF, f.config))
+        {
+            async_return(false);
+        }
+        if (!!(f.config & FDCConfig::DoneMask))
+        {
+            async_return(unsigned(f.config & FDCConfig::DoneMask) >> FDCConfigDoneOffset);
+        }
+        if (!(f.config & FDCConfig::EnableMask))
+        {
+            // no measurement is enabled, hence no measurement can complete
+            async_return(false);
+        }
+    } while (!timeout.Elapsed());
+
+    async_return(false);
+}
+async_end
+
+async(FDC1004::Measure, Timeout timeout)
+async_def(
+    unsigned updated, i;
+    union
+    {
+        struct { uint16_t msbBE, lsbBE; };
+        uint32_t valueBE;
+    };
+)
+{
+    if ((f.updated = await(Wait, timeout)))
+    {
+        for (f.i = 0; f.i < countof(value); f.i++)
+        {
+            if (GETBIT(f.updated, f.i))
+            {
+                if (await(ReadRegister, unsigned(Register::MEAS1_MSB) + 2 * f.i, f.msbBE) &&
+                    await(ReadRegister, unsigned(Register::MEAS1_MSB) + 2 * f.i, f.lsbBE))
+                {
+                    value[f.i] = TO_LE32(f.valueBE) * 1.0f/(1<<19);
+                }
+                else
+                {
+                    RESBIT(f.updated, f.i);
+                }
+            }
+        }
+    }
+
+    async_return(f.updated);
+}
+async_end
+
+}
---
+++ targets/all/sensors/analog/FDC1004.h
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2020 triaxis s.r.o.
+ * Licensed under the MIT license. See LICENSE.txt file in the repository root
+ * for full license information.
+ *
+ * sensors/analog/FDC1004.h
+ */
+
+#pragma once
+
+#include <sensors/I2CSensor.h>
+
+namespace sensors::analog
+{
+
+class FDC1004 : I2CSensor
+{
+public:
+    FDC1004(bus::I2C i2c)
+        : I2CSensor(i2c, Address)
+    {
+    }
+
+    enum Input
+    {
+        CIN1, CIN2, CIN3, CIN4,
+    };
+
+    enum Rate
+    {
+        Rate100Sps = 1 << 10,
+        Rate200Sps = 2 << 10,
+        Rate400Sps = 3 << 10,
+    };
+
+    //! Initializes the sensor
+    async(Init);
+    //! Configures one single ended measurement
+    async(Configure, Input pos, float capdacOffset = 0) { return async_forward(Configure, 0, pos, capdacOffset); }
+    //! Configures one differential measurement
+    async(Configure, Input pos, Input neg ) { return async_forward(Configure, 0, pos, neg); }
+    //! Configures the specified single ended measurement
+    async(Configure, unsigned channel, Input pos, float capdacOffset = 0) { return async_forward(Configure, channel, pos << ChannelConfigPositiveOffset | ChannelConfigNegativeDisabled | ConvertCAPDAC(capdacOffset)); }
+    //! Configures the specified differential measurement
+    async(Configure, unsigned channel, Input pos, Input neg) { return async_forward(Configure, channel, pos << ChannelConfigPositiveOffset | neg << ChannelConfigNegativeOffset); }
+    //! Śets the calibration values for the specified input
+    async(SetCalibration, Input input, float offset = 0, float gain = 1) { return async_forward(SetCalibration, unsigned(input), OffsetAndGain(ConvertOffset(offset), ConvertGain(gain))); }
+    //! Starts single measurements for the specified channel at the specified rate
+    async(StartSingle, Rate rate = Rate100Sps, unsigned index = 0) { return async_forward(Start, FDCConfig(rate | BIT(int(FDCConfigEnableOffset) + index))); }
+    //! Starts repeated measurements at the specified rate
+    async(StartRepeat, Rate rate = Rate100Sps, unsigned mask = 0) { return async_forward(Start, FDCConfig::Repeat | FDCConfig(rate | (mask << FDCConfigEnableOffset))); }
+    //! Stops repeated measurements
+    async(Stop);
+    //! Waits until at least one measurement is completed or the specified timeout elapses
+    //! @returns a bitmask of measurements which are completed
+    async(Wait, Timeout timeout = Timeout::Infinite);
+    //! Retrieves data for all completed measurements
+    //! @returns a bitmask of measurements which have been updated
+    async(Measure, Timeout timeout = Timeout::Infinite);
+
+    //! Retrieves the last measured capacitance for the specified channel
+    float GetCapacitance(unsigned index = 0) const { return value[index]; }
+
+protected:
+    const char* DebugComponent() const { return "FDC1004"; }
+
+private:
+    enum struct Register : uint8_t
+    {
+        MEAS1_MSB = 0x00,
+        MEAS1_LSB = 0x01,
+        MEAS2_MSB = 0x02,
+        MEAS2_LSB = 0x03,
+        MEAS3_MSB = 0x04,
+        MEAS3_LSB = 0x05,
+        MEAS4_MSB = 0x06,
+        MEAS4_LSB = 0x07,
+
+        CONF_MEAS1 = 0x08,
+        CONF_MEAS2 = 0x09,
+        CONF_MEAS3 = 0x0A,
+        CONF_MEAS4 = 0x0B,
+
+        FDC_CONF = 0x0C,
+
+        OFFSET_CAL_CIN1 = 0x0D,
+        OFFSET_CAL_CIN2 = 0x0E,
+        OFFSET_CAL_CIN3 = 0x0F,
+        OFFSET_CAL_CIN4 = 0x10,
+
+        GAIN_CAL_CIN1 = 0x11,
+        GAIN_CAL_CIN2 = 0x12,
+        GAIN_CAL_CIN3 = 0x13,
+        GAIN_CAL_CIN4 = 0x14,
+
+        MFG_ID = 0xFE,
+        DEVICE_ID = 0xFF,
+    };
+
+    enum
+    {
+        DeviceID = 0x1004,          //< Device ID
+        ManufacturerID = 0x5449,    //< Texas Instruments
+        Address = 0x50,             //< Fixed device address
+
+        ChannelConfigPositiveOffset = 13,
+        ChannelConfigNegativeOffset = 10,
+        ChannelConfigCAPDACOffset = 5,
+        ChannelConfigNegativeDisabled = 7 << ChannelConfigNegativeOffset,
+
+        FDCConfigDoneOffset = 0,
+        FDCConfigEnableOffset = 4,
+        FDCConfigRateOffset = 10,
+    };
+
+    enum struct FDCConfig : uint16_t
+    {
+        DoneMask = MASK(4) << FDCConfigDoneOffset,
+        EnableMask = MASK(4) << FDCConfigEnableOffset,
+        Repeat = BIT(8),
+
+        RateMask = MASK(2) << FDCConfigRateOffset,
+
+        Reset = BIT(15),
+    };
+
+    DECLARE_FLAG_ENUM(FDCConfig);
+
+    struct OffsetAndGain
+    {
+        constexpr OffsetAndGain(uint16_t offset, uint16_t gain)
+            : offset(offset), gain(gain) {}
+
+        uint16_t offset;
+        uint16_t gain;
+    };
+
+    async(Configure, unsigned channel, uint16_t cfg);
+    async(SetCalibration, unsigned input, OffsetAndGain offsetAndGain);
+    async(Start, FDCConfig cfg);
+
+    static constexpr float ConvertValue(uint32_t val) { return val * (float)(1.0 / BIT(19)); }
+    static constexpr uint16_t ConvertCAPDAC(float val) { return std::min(unsigned(val * (float)(1.0 / 3.125)), MASK(5)) << ChannelConfigCAPDACOffset; }
+    static constexpr uint16_t ConvertOffset(float val) { return std::min(std::max(signed(val * (float)BIT(11)), -32768), 32767); }
+    static constexpr uint16_t ConvertGain(float val) { return std::min(unsigned(val * (float)BIT(14)), MASK(16)); }
+
+    bool init = false;
+    uint8_t configuredChannels = 0;
+    float value[4] = { NAN, NAN, NAN, NAN };
+};
+
+DEFINE_FLAG_ENUM(FDC1004::FDCConfig);
+
+}
